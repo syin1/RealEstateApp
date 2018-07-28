0de705c2811a45d8d217bcda57a2cdb26a919916
Suraj Jagdev <suraj_57@hotmail.com>
2018-07-28 11:52:55 -0400

    changed nav bar appearance, more importantly on database got the search upto accessing the root ref into the saved house, but appears as object object

--- database.js
+++ database.js
@@ -10,68 +10,6 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $(document).ready(function() {
-  database.ref().on('value', function(childSnapshot) {
-    console.log('css:', childSnapshot.val()); //todo : figure out why null
-
-    var Address = childSnapshot.val().address;
-    var postalCode = childSnapshot.val().postalcode;
-    var Price = childSnapshot.val().price;
-    var numbBeds = childSnapshot.val().beds;
-    var numbBaths = childSnapshot.val().baths;
-    var Description = childSnapshot.val().description;
-    var Type = childSnapshot.val().type;
-    var Utilities = childSnapshot.val().utilities;
-    var Size = childSnapshot.val().size;
-    var searchValue = '';
-
-    /////////////////////////////////////search bar
-    $('.submit').on('click', function() {
-      event.preventDefault();
-      searchValue = $('.input')
-        .val()
-        .trim();
-      console.log('sv:', searchValue);
-
-      //now adding in if search keywords
-      if (
-        this.child('address')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('postalCode')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Price')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Beds')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Baths')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Type')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Utilities')
-          .getValue()
-          .equals(searchValue) ||
-        this.child('Size')
-          .getValue()
-          .equals(searchValue)
-      ) {
-        var card = $('.card-img-top');
-        snapshot.child(searchValue).appendTo(card);
-      }
-      //ended if statement
-
-      return false;
-    });
-  });
-  ///////////////////
-
-  //ended nav bar search on click function
-
-  /////////////////////////////////////////////
   $('#button-id').on('click', function(event) {
     event.preventDefault();
 
@@ -117,15 +55,15 @@ $(document).ready(function() {
 
     database.ref().push(newList);
 
-    console.log(newList.address);
-    console.log(newList.postalcode);
-    console.log(newList.price);
-    console.log(newList.numbBeds);
-    console.log(newList.numbBaths);
-    console.log(newList.description);
-    console.log(newList.type);
-    console.log(newList.utilities);
-    console.log(newList.size);
+    //console.log(newList.address);
+    //console.log(newList.postalcode);
+    //console.log(newList.price);
+    //console.log(newList.numbBeds);
+    //console.log(newList.numbBaths);
+    //console.log(newList.description);
+    //console.log(newList.type);
+    //console.log(newList.utilities);
+    //console.log(newList.size);
 
     alert('Listing successfully added');
 
@@ -141,7 +79,7 @@ $(document).ready(function() {
   });
 
   database.ref().on('child_added', function(childSnapshot) {
-    console.log('val:', childSnapshot.val());
+    //console.log('val:', childSnapshot.val());
 
     var address = childSnapshot.val().address;
     var postalcode = childSnapshot.val().postalcode;
@@ -153,15 +91,15 @@ $(document).ready(function() {
     var utilities = childSnapshot.val().utilities;
     var size = childSnapshot.val().size;
 
-    console.log(address);
-    console.log(postalcode);
-    console.log(price);
-    console.log(numbBeds);
-    console.log(numbBaths);
-    console.log(description);
-    console.log(type);
-    console.log(utilities);
-    console.log(size);
+    //console.log(address);
+    //console.log(postalcode);
+    //console.log(price);
+    //console.log(numbBeds);
+    //console.log(numbBaths);
+    //console.log(description);
+    //console.log(type);
+    //console.log(utilities);
+    //console.log(size);
 
     var newRow = $('<tr>').append(
       $('<td>').text(address),
@@ -177,5 +115,44 @@ $(document).ready(function() {
 
     $('#employee-table > tbody').append(newRow);
   });
+  database.ref().on('value', function(childSnapshot) {
+    console.log('Value objects:', childSnapshot.val()); //todo : figure out why null
+
+    var address = childSnapshot.val().address;
+    var postalCode = childSnapshot.val().postalcode;
+    var price = childSnapshot.val().price;
+    var numbBeds = childSnapshot.val().beds;
+    var numbBaths = childSnapshot.val().baths;
+    var description = childSnapshot.val().description;
+    var type = childSnapshot.val().type;
+    var utilities = childSnapshot.val().utilities;
+    var size = childSnapshot.val().size;
+    var information = '';
+    var searchValue = '';
+
+    var rootRef = database.ref();
+    var urlRef = rootRef.child('real-estate-app-9e4e6');
+    urlRef.once('value', function(child) {
+      childSnapshot.forEach(function(child) {
+        console.log('child: ' + child.val());
+      });
+    });
+    /////////////////////////////////////search bar
+    $('.submit').on('click', function() {
+      event.preventDefault();
+      information = '';
+      searchValue = $('.input')
+        .val()
+        .trim();
+      console.log(searchValue);
+
+      return false;
+    });
+  });
+  ///////////////////
+
+  //ended nav bar search on click function
+
+  /////////////////////////////////////////////
   //end of document.ready function
 });
