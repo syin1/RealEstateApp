075358bbf430a3d1b87a9e6489b745ffd3088812
VishnuSwaminathan <vishcodes@gmail.com>
2018-07-28 12:04:38 -0400

    secondCommit

--- assets/javascript/listing.js
+++ assets/javascript/listing.js
@@ -49,3 +49,30 @@ database
 $('#home').on('click', function() {
   location.href = 'index.html';
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myButton');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal
+$('#myButton').on('click', function() {
+  console.log('clicked agent');
+  //modal.style.display = 'block';
+});
+
+// When the user clicks on <span> (x), close the modal
+$('<span>').on('click', function() {
+  modal.style.display = 'none';
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
