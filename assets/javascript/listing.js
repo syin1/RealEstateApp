238085f4cf45fcd97aa6a4c5c2c9077da73e08ee
VishnuSwaminathan <vishcodes@gmail.com>
2018-07-28 10:31:13 -0400

    firstCommit

--- assets/javascript/listing.js
+++ assets/javascript/listing.js
@@ -1,43 +1,50 @@
-var newList = {
-  Address: Address,
-  postalCode: postalCode,
-  Price: Price,
-  Beds: numbBeds,
-  Baths: numbBaths,
-  Description: Description,
-  Type: Type,
-  Utilities: Utilities,
-  Size: Size
-};
+// var newList = {
+//   Address: address,
+//   postalCode: postalCode,
+//   Price: price,
+//   Beds: beds,
+//   Baths: baths,
+//   Description: description,
+//   Type: type,
+//   Utilities: utilities,
+//   Size: size
+// };
 var config = {
   apiKey: 'AIzaSyAGwvEkUM7qJKhTtJfwo9cAKdCOhrD8lmc',
-  authDomain: 'realestateapp-70bdf.firebaseapp.com',
-  databaseURL: 'https://realestateapp-70bdf.firebaseio.com',
-  projectId: 'realestateapp-70bdf',
-  storageBucket: 'realestateapp-70bdf.appspot.com',
+  authDomain: 'real-estate-app-9e4e6.firebaseapp.com',
+  databaseURL: 'https://real-estate-app-9e4e6.firebaseio.com/',
+  projectId: 'real-estate-app-9e4e6',
+  storageBucket: 'real-estate-app-9e4e6.appspot.com',
   messagingSenderId: '563256383606'
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+database
+  .ref()
+  .orderByChild('address')
+  .equalTo(/**/)
+  .once('child_added', function(childSnapshot) {
+    var Address = childSnapshot.val().address;
+    var Baths = childSnapshot.val().baths;
+    var Beds = childSnapshot.val().beds;
+    var Description = childSnapshot.val().description;
+    var postalCode = childSnapshot.val().postalCode;
+    var Price = childSnapshot.val().price;
+    var Type = childSnapshot.val().type;
+    var Utilities = childSnapshot.val().utilities;
+    var Size = childSnapshot.val().size;
 
-//Size
-var newSize = $('#size').html(newList.Size);
-//Price
-var newPrice = $('#price').html(newList.Price);
-//Beds
-var newBeds = $('#beds').html(newList.Beds);
-//Description
-var newDescription = $('#description').html(newList.Description);
-//Type
-var newType = $('#type').html(newList.Type);
-//Utilities
-var newUtlities = $('#utilities').html(newList.Utilities);
-//PostalCode
-var newPostalCode = $('#postalCode').html(newList.postalCode);
-//Address
-var newAddress = $('#address').html(newList.Address);
-//Baths
-var newBaths = $('#baths').html(newList.Baths);
+    $('#address').html(Address);
+    $('#bath').html(Baths);
+    $('#beds').html(Beds);
+    $('#description').html(Description);
+    $('#postalCode').html(postalCode);
+    $('#price').html(Price);
+    $('#type').html(Type);
+    $('#utilities').html(Utilities);
+    $('#size').html(Size);
+  });
+
 //Back Button
 $('#home').on('click', function() {
   location.href = 'index.html';
