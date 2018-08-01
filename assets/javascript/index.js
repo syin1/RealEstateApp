68a43d0839610d660bcdaaac0651e2c1609b0dd2
Suraj Jagdev <suraj_57@hotmail.com>
2018-08-01 16:54:23 -0400

    added in bootstrap index.js to display search

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -14,6 +14,68 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
+  var search = function() {
+    var row = $('<div>').addClass('row');
+    var colmd = $('<div>').addClass('col-md-10');
+    var card = $('<div>')
+      .addClass('card')
+      .css('width', '18rem');
+    var img = $('<img>')
+      .addClass('card-img-top')
+      .attr(
+        'src',
+        'assets/images/' +
+          imageno +
+          ".jpg' height='200' width='200' alt='Card image cap'>"
+      );
+    var cardBody = $('<div>');
+    var h1 = $('<h1>')
+      .addClass('card-title-address')
+      .append(child.val().address);
+    var h2 = $('<h7>')
+      .addClass('card - subtitle - postalcode')
+      .append(child.val().postalcode);
+    var h3 = $('<h2>')
+      .addClass('card-subtitle mb-2 text-muted ')
+      .append(child.val().price);
+    var h4 = $('<h3>')
+      .addClass('card-subtitle-beds ')
+      .append(child.val().numbBaths);
+    var h5 = $('<h4>')
+      .addClass('card-subtitle-baths')
+      .append(child.val().numbBaths);
+    var h6 = $('<h6>')
+      .addClass('card-subtitle-type')
+      .append(child.val().type);
+    var h7 = $('<h7>')
+      .addClass('card-subtitle-utilities')
+      .append(child.val().utilities);
+    var h8 = $('<h8>')
+      .addClass('card-subtitle-size ')
+      .append(child.val().size);
+    var paragraph = $('<p>')
+      .addClass('card-text-description')
+      .append(child.val().description);
+    var ahref = $('<a>')
+      .attr('href', '#')
+      .addClass('button-id');
+    h1.appendTo(cardBody);
+    h2.appendTo(cardBody);
+    h3.appendTo(cardBody);
+    h4.appendTo(cardBody);
+    h5.appendTo(cardBody);
+    h6.appendTo(cardBody);
+    h7.appendTo(cardBody);
+    h8.appendTo(cardBody);
+    paragraph.appendTo(cardBody);
+    ahref.appendTo(cardBody);
+    img.appendTo(card);
+    cardBody.appendTo(card);
+    card.appendTo(colmd);
+    colmd.appendTo(row);
+  };
+  search();
+
   function renderHTML(snapshot, imageno) {
     var obj = snapshot.val();
 
