ede94d8cb3e45bf6cba193ba5beaab836b4a855d
VishnuSwaminathan <vishcodes@gmail.com>
2018-08-02 04:20:04 -0400

    thirdCommit

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,11 +1,19 @@
 //FIREBASE INIT START
+// var config = {
+//   apiKey: 'AIzaSyBxZrsKG_py7bXZATlHpYoxe8-G9dFmneE',
+//   authDomain: 'test-3c5cc.firebaseapp.com',
+//   databaseURL: 'https://test-3c5cc.firebaseio.com',
+//   projectId: 'test-3c5cc',
+//   storageBucket: 'test-3c5cc.appspot.com',
+//   messagingSenderId: '903703720217'
+// };
 var config = {
-  apiKey: 'AIzaSyBxZrsKG_py7bXZATlHpYoxe8-G9dFmneE',
-  authDomain: 'test-3c5cc.firebaseapp.com',
-  databaseURL: 'https://test-3c5cc.firebaseio.com',
-  projectId: 'test-3c5cc',
-  storageBucket: 'test-3c5cc.appspot.com',
-  messagingSenderId: '903703720217'
+  apiKey: 'AIzaSyDYI4Fjm_RWQadwHH7KdTJGSErId9aUJKY',
+  authDomain: 'real-estate-app-9e4e6.firebaseapp.com',
+  databaseURL: 'https://real-estate-app-9e4e6.firebaseio.com',
+  projectId: 'real-estate-app-9e4e6',
+  storageBucket: 'real-estate-app-9e4e6.appspot.com',
+  messagingSenderId: '1087918608727'
 };
 firebase.initializeApp(config);
 var databaseRef = firebase.database().ref();
